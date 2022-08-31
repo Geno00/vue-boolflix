dcc287862a43e6b7fb13a76b12efe0b2c743c9a5
Andrea Genovese <andregenovese2000@gmail.com>
2022-08-31 15:09:02 +0200

    vue-boolflix

---
+++ src/components/Myheader.vue
@@ -0,0 +1,21 @@
+<template>
+
+    <header>
+        <h2>BOOLFLIX</h2>
+
+        <div>
+            <input type="search" id="search" placeholder="Search..." />
+        </div>
+    </header>
+  
+</template>
+
+<script>
+export default {
+    name: 'Myheader'
+}
+</script>
+
+<style>
+
+</style>
